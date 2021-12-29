985c0efcf4e4be24d60e99fa9a0309b69ed0c924
KimEunJeong77 <48349998+KimEunJeong77@users.noreply.github.com>
2021-12-29 16:10:35 +0900

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './Home'
 import About from './About'
-import Movie from './Movie'
 export default createRouter({
     history : createWebHashHistory(),
     routes : [
